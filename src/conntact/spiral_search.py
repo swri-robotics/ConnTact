8ff3ed7a142f4f984107b17446185e4eb3b8385c
John Bonnin <john.bonnin@swri.org>
2022-08-18 15:19:49 -0500

    Cleaned up ROS-related stuff in spiral search. It's all gone now.

--- src/conntact/spiral_search.py
+++ src/conntact/spiral_search.py
@@ -1,12 +1,8 @@
 # Copyright 2021 Southwest Research Institute
 # Licensed under the Apache License, Version 2.0
 
-# Imports for ros
-
-import rospy
-from geometry_msgs.msg import WrenchStamped, Wrench, TransformStamped, PoseStamped, Pose, Point, Quaternion, Vector3, Transform
 from conntact.assembly_algorithm_blocks import AlgorithmBlocks, AssemblyStep
-from conntact.ros_conntact_interface import ConntactROSInterface
+from conntact.conntact_interface import ConntactInterface
 from transitions import Machine
 
 IDLE_STATE           = 'state_idle'
@@ -17,7 +13,6 @@ INSERTING_PEG_STATE  = 'state_inserting_along_axis'
 COMPLETION_STATE     = 'state_completed_insertion'
 EXIT_STATE           = 'state_exit'
 SAFETY_RETRACT_STATE = 'state_safety_retraction' 
-
 
 #Trigger names
 CHECK_FEEDBACK_TRIGGER     = 'check loadcell feedback'
@@ -30,13 +25,9 @@ SAFETY_RETRACTION_TRIGGER  = 'retract to safety'
 RESTART_TEST_TRIGGER       = 'restart test'
 RUN_LOOP_TRIGGER           = 'run looped code'
 
-
 class SpiralSearch(AlgorithmBlocks, Machine):
-
     def __init__(self, ROS_rate, conntext, interface):
-
         AlgorithmBlocks.__init__(self, ROS_rate, conntext, interface)
-
         #Override Alg Blocks config variables:
         states = [
             IDLE_STATE, 
@@ -47,9 +38,6 @@ class SpiralSearch(AlgorithmBlocks, Machine):
             COMPLETION_STATE, 
             SAFETY_RETRACT_STATE
         ]
-        # TODO: Implement sub-task while loops as reflexive transitions according to https://github.com/pytransitions/transitions#reflexive-from-multiple-states
-        #TODO: Replace warnings and errors with colorized info according to https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal/3332860#3332860
-        # TODO: Replace all transitions with dest "safety_retract_state" with a single one using 'source':'*'
         transitions = [
             {'trigger':CHECK_FEEDBACK_TRIGGER    , 'source':IDLE_STATE          , 'dest':CHECK_FEEDBACK_STATE   },
             {'trigger':APPROACH_SURFACE_TRIGGER  , 'source':CHECK_FEEDBACK_STATE, 'dest':APPROACH_STATE         },
@@ -60,24 +48,10 @@ class SpiralSearch(AlgorithmBlocks, Machine):
             {'trigger':SAFETY_RETRACTION_TRIGGER , 'source':'*'                 , 'dest':SAFETY_RETRACT_STATE, 'unless':'is_already_retracting' },
             {'trigger':RESTART_TEST_TRIGGER      , 'source':SAFETY_RETRACT_STATE, 'dest':APPROACH_STATE         },
             {'trigger':RUN_LOOP_TRIGGER      , 'source':'*', 'dest':None, 'after': 'run_loop'}
-
         ]
-
         Machine.__init__(self, states=states, transitions=transitions, initial=IDLE_STATE)
-
         self.tcp_selected = 'tip'
 
     def main(self):
-        # TODO: Remove following Sleep, use a rospy.wait command of some kind
-        rospy.sleep(3)
-        rospy.wait_for_message("cartesian_compliance_controller/ft_sensor_wrench", WrenchStamped)
-        
         self.algorithm_execute()
-        rospy.loginfo("Spiral Search all done!")
-
-if __name__ == '__main__':
-    
-    assembly_application = SpiralSearch()
-
-    assembly_application.main()
-    
+        self.interface.send_info("Spiral Search all done!")
